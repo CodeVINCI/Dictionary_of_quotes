adaccfed7dd714ac07669f26bbf1a61ee0938db1
CodeVINCI <myc.abhi96@gmail.com>
2018-08-15 03:14:03 +0530

    added a dictionary_of_quotes file

---
+++ dictionary_of_quotes.py
@@ -0,0 +1,4 @@
+dictionary_of_quotes = {"Willian Blake":["If the doors of perception were to be cleaned, everything would appear to man as it is, infinite"],
+                        "Confucius":["Shall I tell you what knowledge is? It is to know what one knows and what one does not know."]
+
+                       }
