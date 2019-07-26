998a83df0394abda33faa216908f8a44fe8534b7
Abhishek <myc.abhi96@gmail.com>
2019-07-26 19:43:54 +0530

    Daniel Kahneman from thinking fast and slow.

--- dictionary_of_quotes.py
+++ dictionary_of_quotes.py
@@ -1,4 +1,5 @@
 dictionary_of_quotes = {"Willian Blake":["If the doors of perception were to be cleaned, everything would appear to man as it is, infinite"],
-                        "Confucius":["Shall I tell you what knowledge is? It is to know what one knows and what one does not know."]
+                        "Confucius":["Shall I tell you what knowledge is? It is to know what one knows and what one does not know."],
+                        "Daniel Kahneman":["We can be blind to the obvious, and are also blind to our blindness."]
 
                        }
